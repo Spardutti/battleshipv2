f5b5a6fd0d3ddc62cea47d08da449aa47b33aec2
spardutti <luisdamian.sp@gmail.com>
2021-02-04 07:24:19 -0300

    need to place ships from an array

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,61 +1,79 @@
 import shipFactory from "./ShipFactory";
 
-const gameBoard = (col, row) => {
+const gameBoard = (cols, rows) => {
   return {
-    board: new Array(row).fill().map(() => new Array(col).fill("sea")),
+    board: new Array(rows).fill().map(() => new Array(cols).fill("sea")),
+    ships: [shipFactory(3)],
+    //----------------------------PLACESHIP
     placeShip() {
-      let ship = shipFactory(3);
+      //variables
       let col, row;
       let shipPlaced = false;
       let horizontal = true;
       let rndNum = Math.floor(Math.random() * 10);
 
-      if (rndNum > 5) {
-        horizontal = false;
-      }
-      while (!shipPlaced) {
-        //--------------------------- HORIZONTAL
-        if (horizontal) {
-          col = Math.floor(
-            Math.random() * (this.board.length - ship.hitPoints.length)
-          );
-          row = Math.floor(Math.random() * this.board.length);
-          if (this.board[col][row] === "sea") {
-            let freeSpaces = 1;
-            for (let i = 1; i < ship.hitPoints.length; i++) {
-              if (this.board[col + i][row] === "sea") {
-                freeSpaces++;
-              }
-                else  return shipPlaced = false;
-            }
-            if (freeSpaces === ship.hitPoints.length) {
-              for (let i = 0; i < ship.hitPoints.length; i++) {
-                this.board[col + i][row] = ship;
-                return (shipPlaced = true);
-              }
-            }
-          } //-----------------------------VERTICAL--//
+      this.ships.forEach((ship, index) => {
+        if (rndNum > 5) {
+          horizontal = false;
         }
-        if (!horizontal) {
-          col = Math.floor(Math.random() * this.board.length);
-          row = Math.floor(
-            Math.random() * (this.board.length - ship.hitPoints.length)
-          );
-          if (this.board[col][row] === "sea") {
-            let freeSpaces = 1;
-            for (let i = 1; i < ship.hitPoints.length; i++) {
-              if (this.board[col][row + i] === "sea") {
-                freeSpaces++;
-              } else return shipPlaced = false;
-            }
-            if (freeSpaces === ship.hitPoints.length) {
-              for (let i = 0; i < ship.hitPoints.length; i++) {
-                this.board[col][row + i] = ship;
+        while (!shipPlaced) {
+          //--------------------------- HORIZONTAL--//
+          if (horizontal) {
+            //check for roll to not place the ship out of boundaries
+            col = Math.floor(
+              Math.random() * (this.board.length - ship.hitPoints.length)
+            );
+            row = Math.floor(Math.random() * this.board.length);
+            //check if space rolled is empty
+            if (this.board[col][row] === "sea") {
+              //create a freeSpaces var to check if there is enough spaces for the length
+              //of the ship
+              let freeSpaces = 1;
+              for (let i = 1; i < ship.hitPoints.length; i++) {
+                if (this.board[col + i][row] === "sea") {
+                  freeSpaces++;
+                } else return (shipPlaced = false);
+              }
+              if (freeSpaces === ship.hitPoints.length) {
+                for (let i = 0; i < ship.hitPoints.length; i++) {
+                  this.board[col + i][row] = ship;
+                }
+                return (shipPlaced = true);
+              }
+            } //-----------------------------VERTICAL--//
+          }
+          if (!horizontal) {
+            col = Math.floor(Math.random() * this.board.length);
+            row = Math.floor(
+              Math.random() * (this.board.length - ship.hitPoints.length)
+            );
+            if (this.board[col][row] === "sea") {
+              let freeSpaces = 1;
+              for (let i = 1; i < ship.hitPoints.length; i++) {
+                if (this.board[col][row + i] === "sea") {
+                  freeSpaces++;
+                } else return (shipPlaced = false);
+              }
+              if (freeSpaces === ship.hitPoints.length) {
+                for (let i = 0; i < ship.hitPoints.length; i++) {
+                  this.board[col][row + i] = ship;
+                }
                 return (shipPlaced = true);
               }
             }
-          }
-        } else return (shipPlaced = false);
+          } else return (shipPlaced = false);
+        }
+      });
+      //roll for aligment
+    },
+    receiveAttack(col, row) {
+      if (this.board[col][row] === "sea") {
+        this.board[col][row] = "attacked";
+        //next player
+        //remove eventListener
+      }
+      //TODO HERE
+      if (this.board === this.ships) {
       }
     },
   };
--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -1,15 +1,17 @@
 import gameBoard from "../Components/GameBoard";
 
+let board = gameBoard(3, 3);
+board.placeShip();
+
+test("used to check the board", () => {
+    expect(board).toBe();
+  });
+  
 
 test("create board", () => {
-    expect(gameBoard(2,2).board.length).toBe(2)
-})
-
+  expect(gameBoard(2, 2).board.length).toBe(2);
+});
 
 test("ship placement", () => {
-    expect(gameBoard(3,3).placeShip()).toBe(true);
-})
-
-test("test for ship larger than free space", () => {
-    expect(gameBoard(2,2).placeShip()).toThrowError();
-})
+  expect(gameBoard(3, 3).placeShip()).toBe(true);
+});
